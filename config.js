9c6b01afda1866aa984a21d5cfd05299b74edaa0
hohp12 <132784727+hohp12@users.noreply.github.com>
2024-01-11 12:01:28 +0400

    Update config.js

--- config.js
+++ config.js
@@ -7,8 +7,8 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['917605902011', 'GURU', true],
-  ['6281255369012', 'Ash', true], 
+  ['+96871518645', 'TOG', true],
+  ['+96871518645', 'Ash', true], 
   [''] 
 ] //Number of owners
 
